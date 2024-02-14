7ba2ddc976ee3b01057e71d3a099a8e8a6a3e471
laureano.deluca <laureano.deluca@conicet.gov.ar>
2024-02-14 16:00:12 -0300

    Update readme

--- scripts/packages/codeblocks.sh
+++
@@ -1,8 +0,0 @@
-##!/usr/bin/pkexec /bin/bash
-
-pkexec apt-get install codeblocks -y | zenity --progress --pulsate --no-cancel --auto-kill --auto-close
-
-if [ "$?" = "0" ]; then
-	zenity --info \
-		--text="Instalación finalizada"
-fi;
--- scripts/packages/lazarus.sh
+++
@@ -1,8 +0,0 @@
-##!/usr/bin/pkexec /bin/bash
-
-pkexec apt-get install lazarus -y | zenity --progress --pulsate --no-cancel --auto-kill --auto-close
-
-if [ "$?" = "0" ]; then
-	zenity --info \
-		--text="Instalación finalizada"
-fi;
