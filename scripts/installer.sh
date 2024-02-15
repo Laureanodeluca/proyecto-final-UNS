9eb4c0f59938411a57fb82d8090fb9f0e5e8bee2
laureano.deluca <laureano.deluca@conicet.gov.ar>
2024-02-15 19:42:57 -0300

    Paso a YAD + BlueJ

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-choice=$(zenity --list \
+choice=$(yad --list \
 	--title "Instalación de paquetes - UNS" \
 	--height=600 \
 	--width=800 \
+	--borders=15 \
 	--hide-column=1 \
 	--column="package_name" --column="Nombre" --column="Descripción" \
+	--print-column=1 \
+	--separator=""\
 	codeblocks CodeBlocks "IDE para programación en C" \
 	lazarus Lazarus "IDE para programación en Pascal"\
-	swi-prolog SWI-prolog "Intérprete para el lenguaje de programación Prolog")
+	swi-prolog SWI-prolog "Intérprete para el lenguaje de programación Prolog"\
+	bluej BlueJ "IDE para programación en Java. Orientado a principiantes.")
 
-
-echo "Seleccionado: ${choice}"
-
-if [[ "$?" != "0" ]] ; then
-	exit 1
-else
+if [ $? -eq 0 ]; then
 	make ${choice} -f $(dirname $0)/packages/makefile
+else
+	exit 1
 fi
