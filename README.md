a7e7efc505fde6da60a56125d9c12cbf9ae7bfb8
laureanodeluca <laureano9asd@gmail.com>
2024-03-04 16:00:43 -0300

    Mejoras: - Agregado año en el que se usa el programa - Agregada materia en el que se usa el programa - Soporte de desinstalación para programas varios.

--- README.md
+++ README.md
@@ -19,21 +19,21 @@ En los siguientes cuadros están los softwares necesarios en la carrera y el sop
 | 3° | Alloy | Métodos Formales | ✔️ | ✔️ | |
 | 3° | VirtualBox | Sistemas Operativos | ✔️ | ✔️ | |
 | 4° | Android Studio | Arqui y Diseño | ✔️ | ✔️ | |
-| 4° | IntelliJ IDEA | Arqui y Diseño | ✔️ | ❌ | |
+| 4° | IntelliJ IDEA | Arqui y Diseño | ✔️ | ✔️ | |
 | 4° | Jira | Arqui y Diseño | ❌ | ❌ | _Imposible de automatizar_ |
 | 4° | Kotlin | Arqui y Diseño | ✔️ | ✔️ | |
-| 4° | Visual Studio Code | IAWeb | ✔️ | ❌ | |
+| 4° | Visual Studio Code | IAWeb | ✔️ | ✔️ | |
 | 4° | NPM | IAWeb | ✔️ | ✔️ | |
 | 4° | React | IAWeb | ✔️ | ✔️ | _No es necesario instalar, se  usa NPM_ |
 | 4° | Vue | IAWeb | ✔️ | ✔️ | _No es necesario instalar, se usa NPM_ |
 | 4° | PHP | IAWeb | ✔️ | ✔️ | |
 | 4° | PostgreSQL | IAWeb | ✔️ | ✔️ | |
 | 4° | NodeJS | IAWeb | ✔️ | ✔️ | _Se instala al instalar npm_ |
-| 4° | Postman | IAWeb, VyVS | ✔️ | ❌ | |
+| 4° | Postman | IAWeb, VyVS | ✔️ | ✔️ | |
 | 4° | ProjectLibre | PSS | ✔️ | ✔️ | |
-| 4° | PMD | VyVS | ✔️ | ❌ | |
+| 4° | PMD | VyVS | ✔️ | ✔️ | |
 | 4° | SonarQube | VyVS | ❌ | ❌ | _Imposible de automatizar_ |
 | 4° | Selenium IDE / WebDriver | VyVS | ✔️ | ✔️ | _Es una extensión de Google Chrome_ |
 | 4° | TAPIR | VyVS | ✔️ | ✔️ | _Es una extensión de Eclipse_ |
-| 4° | Jenkins | VyVS | ✔️ | ❌ | |
+| 4° | Jenkins | VyVS | ✔️ | ✔️ | |
 | 4° | Newman | VyVS | ✔️ | ✔️ | _No es necesario instalar, se usa NPM_ |
--- uns-pacman/main.sh
+++ uns-pacman/main.sh
@@ -15,37 +15,37 @@ do
 	choice=$(yad --list \
 		--title "Instalación de paquetes - UNS" \
 		--height=600 \
-		--width=800 \
+		--width=1200 \
 		--borders=15 \
 		--hide-column=1 \
-		--column="package_name" --column="Nombre" --column="Descripción" \
+		--column="package_name" --column="Nombre" --column="Descripción" --column="Año" --column="Materia/s" \
 		--button="Seleccionar":0 \
 		--button="Salir":1 \
 		--print-column=1 \
 		--grid-lines=vert \
 		--header-tips \
 		--separator="" \
-		codeblocks CodeBlocks "IDE para programación en C." \
-		lazarus Lazarus "IDE para programación en Pascal."\
-		swi-prolog SWI-prolog "Intérprete para el lenguaje de programación Prolog."\
-		bluej BlueJ "IDE para programación en Java. Orientado a principiantes." \
-		eclipse Eclipse "IDE para programación en Java."\
-		staruml StarUML "Herramienta para creación de diagramas UML."\
-		logisim Logisim "Herramienta para diagramado de circuitos."\
-		postgresql PostgreSQL "Sistema de gestión de bases de datos open source."\
-		kotlin Kotlin "Lenguaje de programación de alto nivel."\
-		android-studio "Android Studio" "IDE para programación en dispositivos Android."\
-		visual-studio-code "Visual Studio Code" "IDE para programación multilenguaje."\
-		alloy Alloy "Lenguaje para expresar estructuras de control y comportamiento en un sistema de software."\
-		mysql MySQL "Administrador de bases de datos."\
-		virtualbox VirtualBox "Gestor de máquinas virtuales."\
-		intellij "IntelliJ IDEA" "IDE para programación en Java y Kotlin."\
-		npm "NPM" "Sistema de gestión de paquetes para NodeJS."\
-		projectlibre "ProjectLibre" "Sistema de gestión de proyectos."\
-		postman "Postman" "Gestor de endpoints de APIs."\
-		php "PHP" "Lenguaje de programación interpretado del lado del servidor."\
-		pmd "PMD" "Analizador de código multilenguaje."\
-		jenkins "Jenkins" "Servidor de automatización de código abierto.");
+		lazarus Lazarus "IDE para programación en Pascal." 1° "Resolución de Problemas y Algoritmos" \
+		bluej BlueJ "IDE para programación en Java. Orientado a principiantes." 1° "Introducción a la Programación Orientada a Objetos" \
+		eclipse Eclipse "IDE para programación en Java." 2° "Estructuras de Datos, Tecnología de la Programación" \
+		codeblocks CodeBlocks "IDE para programación en C." 2° "Organización de Computadoras" \
+		staruml StarUML "Herramienta para creación de diagramas UML." 2° "Modelos de Software" \
+		logisim Logisim "Herramienta para diagramado de circuitos." 3° "Arquitectura de Computadoras" \
+		mysql MySQL "Administrador de bases de datos." 3° "Bases de Datos" \
+		swi-prolog SWI-prolog "Intérprete para el lenguaje de programación Prolog." 3° "Lógica para Ciencias de la Computación" \
+		alloy Alloy "Lenguaje para expresar estructuras de control y comportamiento en un sistema de software." 3° "Métodos Formales para Ingeniería de Software" \
+		virtualbox VirtualBox "Gestor de máquinas virtuales." "3°, 5°" "Sistemas Operativos, Redes de Computadoras" \
+		postgresql PostgreSQL "Sistema de gestión de bases de datos open source." 4° "Ingeniería de Aplicaciones Web" \
+		kotlin Kotlin "Lenguaje de programación de alto nivel." 4° "Arquitectura y Diseño de Software" \
+		android-studio "Android Studio" "IDE para programación en dispositivos Android." 4° "Arquitectura y Diseño de Software" \
+		visual-studio-code "Visual Studio Code" "IDE para programación multilenguaje." 4° "Ingeniería de Aplicaciones Web" \
+		intellij "IntelliJ IDEA" "IDE para programación en Java y Kotlin." 4° "Arquitectura y Diseño de Software" \
+		npm "NPM" "Sistema de gestión de paquetes para NodeJS." 4° "Ingeniería de Aplicaciones Web" \
+		projectlibre "ProjectLibre" "Sistema de gestión de proyectos." 4° "Proyectos de Sistemas de Software" \
+		postman "Postman" "Gestor de endpoints de APIs." 4° "Ingeniería de Aplicaciones Web" \
+		php "PHP" "Lenguaje de programación interpretado del lado del servidor." 4° "Ingeniería de Aplicaciones Web" \
+		pmd "PMD" "Analizador de código multilenguaje." 4° "Verificación y Validación de Software" \
+		jenkins "Jenkins" "Servidor de automatización de código abierto." 4° "Verificación y Validación de Software" );
 
 	exval=$?;
 
