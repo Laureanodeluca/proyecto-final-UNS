85bae6ca40a7618d80dd3b7521e34093990cf9bf
Laureano De Luca <laureano.deluca@conicet.gov.ar>
2024-03-20 09:58:18 -0300

    Fix updater

--- uns-pacman/updater.sh
+++ uns-pacman/updater.sh
@@ -15,6 +15,6 @@ if [ "$LATEST" != "$INSTALLED" ]; then
     if [ $? -eq 0 ]; then 
         URL=$(curl -sL https://api.github.com/repos/Laureanodeluca/proyecto-final-UNS/releases/latest | grep "browser_download_url.*deb"| cut -d : -f 2,3 | tr -d \")
         wget ${URL} -P ~ 2>&1 | grep --line-buffered -Eo "[0-9]{1,2}%" | yad --progress --auto-kill --auto-close --borders=15 --no-buttons --text="Descargando UNS-Pacman..." --title="UNS-Pacman Updater"
-        pkexec dpkg -i ~/uns-pacman_${LATEST}_all.deb | zenity --progress --pulsate --no-cancel --auto-kill --auto-close --width=350 --title="UNS-Pacman Updater" --text="Instalando paquete..."
+        pkexec dpkg -i ~/uns-pacman_${LATEST}_*.deb | zenity --progress --pulsate --no-cancel --auto-kill --auto-close --width=350 --title="UNS-Pacman Updater" --text="Instalando paquete..."
     fi;
 fi;
